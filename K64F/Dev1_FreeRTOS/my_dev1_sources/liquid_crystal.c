432175ee5e067fec1f5ba5931233bfcdbf11e34e
Zedd83 <zkrousky@gmail.com>
2019-12-13 12:58:32 +0000

    lcd files created

---
+++ K64F/Dev1_FreeRTOS/my_dev1_sources/liquid_crystal.c
@@ -0,0 +1,8 @@
+/*
+ * liquid_crystal.c
+ *
+ *  Created on: 13 Dec 2019
+ *      Author: zedd
+ */
+
+
---
+++ K64F/Dev1_FreeRTOS/my_dev1_sources/liquid_crystal.h
@@ -0,0 +1,15 @@
+/*
+ * liquid_crystal.h
+ *
+ *  Created on: 13 Dec 2019
+ *      Author: zedd
+ */
+
+#ifndef LIQUID_CRYSTAL_H_
+#define LIQUID_CRYSTAL_H_
+
+#define DISPLAY 0x38	// 16x2, 7x5 matrix display
+
+void lcdTask(void*);
+
+#endif /* LIQUID_CRYSTAL_H_ */
